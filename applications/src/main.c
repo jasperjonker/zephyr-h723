0f3ea1d8014b3efc5b782dadbed632aaa0788831
Jasper Jonker <j8tjuh@gmail.com>
2023-05-23 17:57:22 +0200

    Use logging module
    Signed-off-by: Jasper Jonker <j8tjuh@gmail.com>

--- applications/src/main.c
+++ applications/src/main.c
@@ -4,14 +4,15 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/drivers/uart.h>
-// #include <zephyr/logging/log.h>
+#include <zephyr/logging/log.h>
 
 /* LOG Module */
-// LOG_MODULE_REGISTER(main);
+LOG_MODULE_REGISTER(main);
 
 /* Function prototypes */
 int8_t init_pwm_led(uint32_t period, uint32_t pulse_width);
 int8_t init_pwm_servo(void);
+int8_t set_pulse_width(uint32_t pulse_width);
 void ramp_servo(const uint32_t pulse_width_start, const uint32_t pulse_width_end, const uint32_t step);
 
 /* PWM LED */
@@ -27,18 +28,18 @@ static const uint32_t max_pulse = DT_PROP(DT_ALIAS(servo0), max_pulse);
 // ------------------------------------------------
 int8_t init_pwm_led(uint32_t period, uint32_t pulse_width) {
     if (!device_is_ready(pwm_led0.dev)) {
-        printk("Cannot find PWM LED device!\n");
+        LOG_ERR("Cannot find PWM LED device!\n");
         return -1;
     } else {
-        printk("PWM LED device found: %s\r\n", pwm_led0.dev->name);
+        LOG_INF("PWM LED device found: %s\r\n", pwm_led0.dev->name);
     }
 
     int ret = pwm_set_dt(&pwm_led0, period, pulse_width);
     if (ret < 0) {
-        printk("Error %d: failed to set pulse width\r\n", ret);
+        LOG_ERR("Error %d: failed to set pulse width\r\n", ret);
         return -1;
     } else {
-        printk("PWM set!\r\n");
+        LOG_INF("PWM set!\r\n");
     }
 
     return 0;
@@ -49,22 +50,38 @@ int8_t init_pwm_led(uint32_t period, uint32_t pulse_width) {
 // ------------------------------------------------
 int8_t init_pwm_servo(void) {
     if (!device_is_ready(servo.dev)) {
-        printk("Cannot find PWM servo device!\n");
+        LOG_ERR("Cannot find PWM servo device!\n");
         return -1;
     } else {
-        printk("PWM servo device found: %s\r\n", servo.dev->name);
+        LOG_INF("PWM servo device found: %s\r\n", servo.dev->name);
     }
 
-    int ret_init = pwm_set_pulse_dt(&servo, PWM_USEC(1500));
-    // int ret_init = pwm_set(servo.dev, servo.channel, servo.period, PWM_USEC(1500), servo.flags);
-    if (ret_init < 0) {
-        printk("Error Servo init %d: failed to set pulse width\r\n", ret_init);
-        return ret_init;
-    } else {
-        printk("PWM set!\r\n");
+    if (set_pulse_width(PWM_USEC(1500))) {
+        return -1;
     }
 
     return 0;
+}
+
+// ------------------------------------------------
+// Initialize PWM Servo
+// ------------------------------------------------
+int8_t set_pulse_width(uint32_t pulse_width) {
+    // Make sure pulse width is within bounds
+    if (pulse_width < min_pulse || pulse_width > max_pulse) {
+        LOG_ERR("Error: pulse width %d us is out of bounds [%d, %d] us\r\n", pulse_width / 1000, min_pulse / 1000, max_pulse / 1000);
+        return -1;
+    }
+
+    uint8_t ret = pwm_set_pulse_dt(&servo, pulse_width);
+
+    if (ret < 0) {
+        LOG_ERR("Error %d: failed to set pulse width to %d us\r\n", ret, pulse_width / 1000);
+        return ret;
+    } else {
+        LOG_INF("PWM set to %d us\r\n", pulse_width / 1000);
+        return 0;
+    }
 }
 
 void ramp_servo(const uint32_t pulse_width_start, const uint32_t pulse_width_end, const uint32_t step) {
@@ -72,12 +89,8 @@ void ramp_servo(const uint32_t pulse_width_start, const uint32_t pulse_width_end
     int ret;
 
     while (pulse_width != pulse_width_end) {
-        ret = pwm_set_pulse_dt(&servo, pulse_width);
-        if (ret < 0) {
-            printk("Error %d: failed to set pulse width to %d us\r\n", ret, pulse_width / 1000);
+        if (set_pulse_width(pulse_width)) {
             return;
-        } else {
-            printk("PWM set to %d us\r\n", pulse_width / 1000);
         }
 
         if (pulse_width_end >= pulse_width_start) {
@@ -91,7 +104,7 @@ void ramp_servo(const uint32_t pulse_width_start, const uint32_t pulse_width_end
 }
 
 int main(void) {
-    // LOG_INF("Booting...\r\n");
+    LOG_INF("Booting...\r\n");
 
     init_pwm_led((uint32_t)2e9, (uint32_t)1e9);
     init_pwm_servo();
@@ -104,7 +117,7 @@ int main(void) {
         ramp_servo(PWM_USEC(2000), PWM_USEC(900), PWM_USEC(100));
     }
 
-    // LOG_INF("Booting...[DONE]\r\n");
+    LOG_INF("Booting...[DONE]\r\n");
 
     while (1) {
         k_sleep(K_SECONDS(1));
